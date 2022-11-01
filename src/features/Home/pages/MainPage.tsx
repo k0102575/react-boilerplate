9ca5aa337bfd5d4d146bc15382276e02538fb5e6
k0102575 <k0102575@gmail.com>
2022-11-01 14:39:59 +0900

    feat: emotion , craco 적용

--- src/features/Home/pages/MainPage.tsx
+++ src/features/Home/pages/MainPage.tsx
@@ -1,5 +1,12 @@
+import { css } from '@emotion/react';
+
+const bodyStyle = () => css`
+  height: 500px;
+  border: 1px solid black;
+`;
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return <div css={bodyStyle}>MainPage</div>;
 };
 
 export default MainPage;
