ab68e587d561629f4fbd1aa1f9b5519e49dff71f
k0102575 <k0102575@gmail.com>
2022-11-01 15:15:32 +0900

    feat: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NotFoundPage from './components/ErrorPage/NotFoundPage';
 import Container from './features/Layout/Container';
 import MainRoutes from './features/Home/Routes';
 import ProductRoutes from './features/Product/Routes';
 
 const App = () => {
+  // react query
+  const queryClient = new QueryClient();
+
   return (
-    <Routes>
-      <Route element={<Container />}>
-        <Route index element={<MainRoutes />} />
-        <Route path="product/*" element={<ProductRoutes />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<Container />}>
+          <Route index element={<MainRoutes />} />
+          <Route path="product/*" element={<ProductRoutes />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 };
 
