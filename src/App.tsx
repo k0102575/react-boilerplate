cabddf105f37e22017ce5a146a71c2b14f34670e
k0102575 <k0102575@gmail.com>
2022-11-01 15:21:41 +0900

    feat: add recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
 import NotFoundPage from './components/ErrorPage/NotFoundPage';
 import Container from './features/Layout/Container';
 import MainRoutes from './features/Home/Routes';
@@ -11,13 +12,15 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route element={<Container />}>
-          <Route index element={<MainRoutes />} />
-          <Route path="product/*" element={<ProductRoutes />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route element={<Container />}>
+            <Route index element={<MainRoutes />} />
+            <Route path="product/*" element={<ProductRoutes />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </RecoilRoot>
     </QueryClientProvider>
   );
 };
