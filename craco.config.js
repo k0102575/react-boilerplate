9c716892a913122e5999871196b6774822dd7933
theo.kim <theo.kim@stagefive.com>
2024-06-12 12:01:42 +0900

    fix: babel plugin error

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,17 @@
+const path = require('path');
+
 module.exports = {
   babel: {
     presets: [['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }]],
-    plugins: ['@emotion/babel-plugin'],
+    plugins: [
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ],
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
   },
 };
